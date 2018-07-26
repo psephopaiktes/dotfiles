76ada73801912f8284c86e288268f7357858f8d7
Akira HIRATA <hirataakira6@gmail.com>
2018-07-26 19:14:06 +0900

    Update app-init.sh

--- app-init.sh
+++ app-init.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 
-# Node関連
+# Node
 curl -L git.io/nodebrew | perl - setup
 nodebrew install-binary stable
 nodebrew use stable
 brew install yarn --without-node
 
 
-# Synchronous Settings by Google Drive
+# Symbolic link Settings by Google Drive
 
-## Ai Scripts シンボリックリンク設定
-## 要バージョンごとの設定
+## Ai Scripts
+## Need latest version name
 sudo rm -rf "/Applications/Adobe Illustrator CC 2018/Presets.localized/ja_JP/スクリプト"
 sudo ln -snf "/Users/a-hirata/Google Drive/App/ai scripts" "/Applications/Adobe Illustrator CC 2018/Presets.localized/ja_JP"
 sudo mv "/Applications/Adobe Illustrator CC 2018/Presets.localized/ja_JP/ai scripts" "/Applications/Adobe Illustrator CC 2018/Presets.localized/ja_JP/スクリプト"
 
-## VS code シンボリックリンク設定
+## VS code
 sudo rm -rf "/Users/a-hirata/Library/Application Support/Code/User"
 sudo rm -rf "/Users/a-hirata/.vscode"
 sudo ln -snf "/Users/a-hirata/Google Drive/App/VS Code/User" "/Users/a-hirata/Library/Application Support/Code"
 sudo ln -snf "/Users/a-hirata/Google Drive/App/VS Code/.vscode" "/Users/a-hirata"
 
-## itunesのシンボリックリンク設定
+## itunes
 sudo rm -rf "/Users/a-hirata/Music/iTunes"
 sudo ln -snf "/Users/a-hirata/Google Drive/App/iTunes" "/Users/a-hirata/Music"
 
 
 # QuickLook Setting
-## 拡張子のないファイルをテキストプレビュー
+## txt preview if file hasnt ext
 brew cask install qlstephen
 ## Markdown
 brew cask install qlmarkdown
@@ -36,12 +36,12 @@ brew cask install qlmarkdown
 brew cask install quicklook-json
 ## CSV
 brew cask install quicklook-csv
-## コード色分け, 行表示設定 色の設定も可能
+## syntax color, line number
 brew cask install qlcolorcode
 defaults write org.n8gray.QLColorCode extraHLFlags '-l'
-## zipファイルのサムネ表示、クイックルック表示
+## zip
 brew cask install hetimazip
-## 画像表示の際にサイズをタイトルに提示
+## preview img size
 brew cask install qlimagesize
 brew cask install webpquicklook
 ## config
