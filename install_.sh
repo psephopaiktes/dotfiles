1c3945a5876daacf131af81e7dbdcb268f0468dd
Akira HIRATA <hirataakira6@gmail.com>
2019-10-12 02:43:56 +0900

    Rename install.sh to install_.sh

---
+++ install_.sh
@@ -0,0 +1,32 @@
+# node ########################################################
+
+echo "...install NODEs"
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+source ~/.zshenv
+source ~/.zshrc
+nvm i --lts
+nvm use --lts
+
+# Install Global NPM Packages
+npm i -g firebase-tools
+npm i -g @vue/cli
+
+
+# brew ########################################################
+
+echo "...install brew"
+/usr/bin/ruby -e "`curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install`"
+brew bundle --global
+brew cleanup
+brew cask cleanup
+
+
+# other ########################################################
+
+defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write org.n8gray.QLColorCode extraHLFlags '-l'
+defaults write com.apple.finder QLEnableTextSelection -bool true
+qlmanage -r
