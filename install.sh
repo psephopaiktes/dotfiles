439f4e7ce683db7f1fb7ff1cacf0add1cb0e71dc
Akira HIRATA <hirataakira6@gmail.com>
2021-04-12 12:47:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,14 @@ echo "...install NODEs"
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+wait
+
 source ~/.zshenv
 source ~/.zshrc
+
 nvm i --lts
+wait
+
 nvm use --lts
 
 # Install Global NPM Packages
@@ -16,20 +21,29 @@ npm i -g firebase-tools
 npm i -g @vue/cli
 npm i -g vercel
 npm i -g @squoosh/cli
+wait
 
 
 # brew ########################################################
 
 echo "...install brew"
+
 /usr/bin/ruby -e "`curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install`"
+wait
+
 brew bundle --global
+wait
+
 brew cleanup
+wait
 
 
 # SSH ########################################################
 mkdir ~/.ssh
 cd ~/.ssh
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+wait
+
 pbcopy < ~/.ssh/id_rsa.pub
 
 
@@ -40,5 +54,9 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 defaults write org.n8gray.QLColorCode extraHLFlags '-l'
 defaults write com.apple.finder QLEnableTextSelection -bool true
 qlmanage -r
+wait
 
 xcodebuild -license accept  
+
+
+echo "DONE"
