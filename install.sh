08e6c5d6133c946577fb031d63da0a6e23546a27
Akira HIRATA <hirataakira6@gmail.com>
2021-12-16 12:15:00 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 echo "...install NODEs"
 
 # nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 wait
 
 source ~/.zshenv
